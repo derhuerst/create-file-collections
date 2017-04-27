fec26061efdc34612d9632d94b8b381f4cd8cecb
Jannis R <mail@jannisr.de>
2017-04-27 06:06:57 +0200

    basic pipeline

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
 'use strict'
 
-// todo
+const {src} = require('vinyl-fs')
+const path = require('path')
+const through = require('through2')
+const parallel = require('async/parallel')
+
+const createCollections = (dir, computeLinks, put, del) => {
+	return src([
+		path.join(dir, '**')
+	], {read: false})
+
+	.pipe(through.obj(function (file, _, cb) {
+		if (!file.stat.isFile()) return cb()
+		const self = this
+
+		computeLinks(file, (err, links) => {
+			if (err) return cb(err)
+			if (!links) return cb()
+
+			file.links = links
+			self.push(file)
+			cb()
+		})
+	}))
+
+	.pipe(through.obj(function (file, _, cb) {
+		const self = this
+
+		const tasks = file.links.map((link) => put.bind({}, link, file.path))
+		parallel(tasks, (err) => {
+			if (!err) self.push(file)
+			cb(err)
+		})
+	}))
+}
+
+module.exports = createCollections
