1fa5dd60192cbf5d9cff73c5f7d7e8ef9c644302
Jannis R <mail@jannisr.de>
2017-04-28 00:28:49 +0200

    tree cache file

--- example/index.js
+++ example/index.js
@@ -1,15 +1,32 @@
 'use strict'
 
 const path = require('path')
+const fs = require('fs')
 const createCollections = require('..')
 
 const tag = require('../tag/json')
 const {put, del} = require('../apply/hardlinks')
 // const {put, del} = require('../apply/symlinks')
+const treeCache = require('../lib/tree-cache')
+
+const showError = (err) => {
+	if (!err) return
+	console.error(err)
+	process.exit(1)
+}
 
 const src = path.join(__dirname, 'src')
 const dest = path.join(__dirname, 'dest')
 
-createCollections(src, tag, put(dest), del(dest))
-.on('error', console.error)
-.on('data', (file) => console.log(file.basename))
+const cache = treeCache(path.join(__dirname, 'tree.json'))
+
+cache.read((err, tree) => {
+	if (err) return showError(err)
+
+	createCollections(src, tag, put(dest), del(dest), {
+		oldTree: tree,
+		saveTree: (tree) => cache.write(tree, showError)
+	})
+	.on('error', console.error)
+	.on('data', (file) => {})
+})
