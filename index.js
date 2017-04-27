113b04e986594ea9ddf74be827c1bde776d09a3a
Jannis R <mail@jannisr.de>
2017-04-27 17:11:39 +0200

    cache the link tree :zap:

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 'use strict'
 
+const noop = require('lodash/noop')
 const {src} = require('vinyl-fs')
 const path = require('path')
 const through = require('through2')
 const parallel = require('async/parallel')
+const differenceWith = require('lodash/differenceWith')
+const isEqual = require('lodash/isEqual')
 
-const createCollections = (dir, computeLinks, put, del) => {
+const defaults = {
+	oldTree: {},
+	saveTree: noop
+}
+
+const createCollections = (dir, computeLinks, put, del, opt = {}) => {
+	opt = Object.assign({}, defaults, opt)
+	const tree = {}
+
 	return src([
 		path.join(dir, '**')
 	], {read: false})
@@ -17,6 +28,9 @@ const createCollections = (dir, computeLinks, put, del) => {
 		computeLinks(file, (err, links) => {
 			if (err) return cb(err)
 			if (!links) return cb()
+			if (!Array.isArray(links)) {
+				return cb(new Error('links must be an array'))
+			}
 
 			file.links = links
 			self.push(file)
@@ -24,15 +38,34 @@ const createCollections = (dir, computeLinks, put, del) => {
 		})
 	}))
 
+	// todo: targets in opt.oldTree, but not in tree
+
 	.pipe(through.obj(function (file, _, cb) {
 		const self = this
 
-		const tasks = file.links.map((link) => put.bind({}, link, file.path))
+		const newLinks = file.links
+		const oldLinks = opt.oldTree[file.relative] || []
+		const tasks = []
+
+		const toDel = differenceWith(oldLinks, newLinks, isEqual)
+		for (let link of toDel) {
+			tasks.push(del.bind({}, link, file.path))
+		}
+
+		const toPut = differenceWith(newLinks, oldLinks, isEqual)
+		for (let link of toPut) {
+			tasks.push(put.bind({}, link, file.path))
+		}
+
 		parallel(tasks, (err) => {
+			tree[file.relative] = newLinks
+
 			if (!err) self.push(file)
 			cb(err)
 		})
 	}))
+
+	.on('end', () => opt.saveTree(tree))
 }
 
 module.exports = createCollections
